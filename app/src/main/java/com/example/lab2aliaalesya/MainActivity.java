5b73745bde95fa102f51f5e32a906ee23962fa44
Alesya <2022484762@student.uitm.edu.my>
2023-11-09 12:27:39 +0800

    first commit using intent and bg apps

---
+++ app/src/main/java/com/example/lab2aliaalesya/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab2aliaalesya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button openActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        openActivity = (Button) findVIewById(R.id.button);
+    }
+
+    private Object findVIewById(int button)
+    {
+        return null;
+    }
+
+    public void openActivity2(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+}
